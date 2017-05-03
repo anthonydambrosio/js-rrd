e20582012a0a301fdfade70c532c128e707f84ba
Anthony D'Ambrosio <developer@expressoair.com>
2017-05-03 11:36:57 -0400

    First useful version.

--- index.js
+++ index.js
@@ -12,69 +12,107 @@ function seconds( unit, amount ) {
 }
 
 
-function create( unit, count, interval ) {
+function create( unit, count, func ) {
 	var result = new Array( count ).fill( 0 );
-		interval = interval,
+		interval = seconds( unit ) * 1000,
 		index = 0,
-		sum = {
-			total: 0,
-			update: function( data ) {
-				this.total += data;
+		// Data functions.
+		dataFunc = {
+			// Count the number of times the update function
+			// was called for the period.
+			count: {
+				value: 0,
+				update: function( data ) {
+					this.value++;
+				},
+				result: function() {
+					var value = this.value;
+					this.value = 0;
+					return value;
+				}
 			},
-			result: function() {
-				var total = this.total;
-				this.total = 0;
-				return total;
-			}
-		},
-		avg = {
-			total: 0.0,
-			count: 0,
-			update: function( data ) {
-				this.total += data;
-				this.count++;
+			// Return the last value that was supplied to
+			// the update function.
+			last: {
+				value: 0,
+				update: function( data ) {
+					this.value = data;
+				},
+				result: function() {
+					return this.value;
+				}
 			},
-			result: function() {
-				var total = this.total;
-				var count = this.count;
-				this.total = 0.0;
-				this.count = 0;
-				return count == 0 ? 0.0 : total / count;
+			// Sum the data for the period.
+			sum : {
+				total: 0,
+				update: function( data ) {
+					this.total += data;
+				},
+				result: function() {
+					var total = this.total;
+					this.total = 0;
+					return { ts: Math.floor( Date.now() / 1000 ), value: total };
+				}
+			},
+			// Average the data for the period.
+			avg : {
+				total: 0.0,
+				count: 0,
+				update: function( data ) {
+					this.total += data;
+					this.count++;
+				},
+				result: function() {
+					var total = this.total;
+					var count = this.count;
+					this.total = 0.0;
+					this.count = 0;
+					var value = count == 0 ? 0.0 : total / count;
+					return { ts: Math.floor( Date.now() / 1000 ), value: value };
+				}
 			}
-		},
-		operation = avg,
+		}[func || 'sum'],
 		increment = function() {
-			result[ index ] = operation.result();
-			if ( index < result.length-1 )
+			console.log( 'increment: ' + index );
+			if ( index < result.length ) {
+				result[ index ] = dataFunc.result();
 				index += 1;
-			else
-				index = 0;
+			} else {
+				result.push( dataFunc.result() );
+				result.shift();
+			}
 		};
 		var iid = setInterval( increment, interval );
 	return {
 		update: function( data ) {
-			operation.update( data );
+			dataFunc.update( data );
 		},
 		dump: function() {
 			console.dir( result, { depth: null } );
 		},
-		snapshot: fucntion() {
+		fetch: function() {
 			return result;
-		}
+		},
 		stop: function() {
 			clearInterval( iid );
 		}
 	}
 }
 
-var jsrrd = create( 'sec', 30, 10000 );
+var jsrrd = create( 'min', 3, 'count' );
 
-var getMem = function() {
+var updateFunc = function() {
 	var mem = os.freemem();
 	jsrrd.update( mem );
 };
 
-setInterval( getMem, 1000 );
-setInterval( jsrrd.dump, 5000 );
-setTimeout( jsrrd.stop, 35000 );
+var display = function() {
+	var r = jsrrd.fetch();
+	console.log( "Result" );
+	console.dir( r, { depth: null } );
+}
 
+setInterval( updateFunc, 500 );
+setInterval( jsrrd.dump, 1000 );
+setInterval( jsrrd.stop, 61000 );
+setInterval( display, 90000)
